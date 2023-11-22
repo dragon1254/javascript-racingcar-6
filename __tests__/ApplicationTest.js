104afa70eb20365839ef5a06bddde73a56c40bde
unknown <dkvz@naver.com>
2023-11-22 11:06:13 +0900

    기능테스트, 예외테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -45,7 +45,86 @@ describe("자동차 경주 게임", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
+  test("3명 3번 경기", async () => {
+    // given
+    const inputs = ["pobi,woni,drago", "3"];
+    const outputs = ["pobi : ---"];
+    const randoms = [4,3,3,8,7,6,6,1,2];
+    const logSpy = getLogSpy();
 
+    mockQuestions(inputs);
+    mockRandoms([...randoms]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+  test("4명 6번 경기", async () => {
+    // given
+    const inputs = ["pobi,woo,drago,zin", "6"];
+    const outputs = ["zin : ----"];
+    const randoms = [4,3,3,8,7,6,6,1,2,3,4,5,6,7,8,9,7,3,8,1,3,6,1,7];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([...randoms]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
+
+describe("예외 테스트", () => {
+  test("문자 입력횟수 에 대한 예외 처리", async () => {
+
+    const inputs = ["pobi,woni,drago", "3a"]
+
+    // given
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+  test("비정수 입력 횟수에 대한 예외 처리", async () => {
+
+    const inputs = ["pobi,woni,drago", "3.4"]
+
+    // given
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+  test("1 이하 입력 횟수에 대한 예외 처리", async () => {
+
+    const inputs = ["pobi,woni,drago", "-4"]
+
+    // given
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
   test.each([
     [["pobi,javaji"]],
     [["pobi,eastjun"]]
